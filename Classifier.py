bd4945d79f1de65f52f26d4599f0f4aa22f025b2
Uddipaan Hazarika <uddipaanh369@gmail.com>
2018-02-07 16:00:27 +0530

    Training part

---
+++ Classifier.py
@@ -0,0 +1,30 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import Pipeline
+from ME import *
+from sklearn.externals import joblib
+
+#Pipeline class is a useful toll for encapsulating multiple different transforms alongside an estimator into one obj.
+clsf = Pipeline([
+	('vectorizer', DictVectorizer(sparse=False)),
+	('classifier', DecisionTreeClassifier(criterion='entropy'))
+])
+#We call our estimator instance clf, as it is a classifier. It now must be fitted to the model, i.e., it must learn from the model which is done by passing the training set to the fit method.
+clsf.fit(x[:1000], y[:1000])
+
+print "Training complete!"
+
+x_test, y_test = transform_to_dataset(test)
+
+print "Accuracy: ", clsf.score(x_test, y_test)
+
+#Saving the model so that i dont have to train it every time i run the program.
+filename = 'ME_Model.sav'
+joblib.dump(clsf,filename)
+
+##The class DictVectorizer can be used to convert feature arrays represented as list of stndrd Python "dict" objects to NumPy/SciPy representations used by scikit-learn estimators.
+'''vec = DictVectorizer()
+t = vec.fit_transform(x[:5]).toarray()
+print t
+r = vec.get_feature_names()
+print r'''
--- ME.py
+++ ME.py
@@ -1,5 +1,53 @@
 import nltk
+import pprint
 
-tagged_sentences = nltk.corpus.treebank.tagged_sents()
+#The corpora is assigned to "tagged_sentence".
+tagged_sentences = nltk.corpus.brown.tagged_sents()
 
-print tagged_sentences[0]
+#Displays the sentences under consideration.
+#This will be used to train the ME tagger.
+##print tagged_sentences[0]
+
+
+#this removes the POS tags in the training corpora
+def untag(tagged_sentences):
+	return [w for w, t in tagged_sentences]
+
+#Extracting the features(hassle incoming -_- )
+def features(sentence, index):
+	##print('\n')
+	return {
+		'word': sentence[index],
+		'first_word': index == 0,
+		'last_word': index == len(sentence) - 1,
+		'prev-word': '' if index == 0 else sentence[index-1],
+		'next-word': '' if index == len(sentence) - 1 else sentence[index + 1]
+}
+##pprint.pprint(features(untag(tagged_sentences[0]),0))
+
+#split the training corpus into training sentences and testing sentences
+def split(sentences):
+	cutoff = int(0.75 * len(sentences))
+	training = sentences[:cutoff]
+	test = sentences[cutoff:]
+	
+	
+	return training, test
+
+training, test = split(tagged_sentences)
+
+#transform the training sentences to a dataset
+#in the dataset list x defines the word in terms of the feature and list y defines the POS tag of the feature
+#this transformation enables us to train the classifier
+def transform_to_dataset(sentences):
+	x,y = [],[]
+	
+	for sentc in sentences:
+		for index in range(len(sentc)):
+			x.append(features(untag(sentc), index))
+			y.append(sentc[index][1])
+	return x, y
+
+x,y = transform_to_dataset(training)
+
+
